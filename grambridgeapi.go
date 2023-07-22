c34582adba801b4a9a59b78ce748d795caf2472d
Simon Tucker <brotherlogic@gmail.com>
2023-07-21 23:31:51 -0700

    First pass

---
+++ grambridgeapi.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+
+	rcpb "github.com/brotherlogic/recordcollection/proto"
+)
+
+// ClientUpdate on an updated record
+func (s *Server) ClientUpdate(ctx context.Context, req *rcpb.ClientUpdateRequest) (*rcpb.ClientUpdateResponse, error) {
+
+	return &rcpb.ClientUpdateResponse{}, nil
+}
